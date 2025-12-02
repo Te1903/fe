8a639dc925ccb766cfee9145667d24d4fc3ea465
Te1903 <letuanem803@gmail.com>
2025-12-03 05:51:10 +0700

    update code

--- js/tables-admin.js
+++ js/tables-admin.js
@@ -6,7 +6,7 @@ const listBox = document.getElementById("tableList");
 function renderTables() {
     const data = JSON.parse(localStorage.getItem(APP_KEY)) || {};
 
-    const tables = data.tables || [1,2,3,4,5,6]; // Nếu chưa có thì tạo 6 bàn mẫu
+    const tables = data.tables || [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]; // Nếu chưa có thì tạo 6 bàn mẫu
 
     listBox.innerHTML = tables.map(n => `
         <div class="table-item">
