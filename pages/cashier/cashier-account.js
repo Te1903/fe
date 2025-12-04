adf9c5848bf520506d86eb9d57131fcceed9af53
Te1903 <letuanem803@gmail.com>
2025-12-04 23:23:43 +0700

    update code

--- pages/cashier/cashier-account.js
+++ pages/cashier/cashier-account.js
@@ -3,8 +3,6 @@ const cashierAccount = {
   name: "Trương Nguyễn Hoài Anh",
   username: "Cashier",
   role: "Thu ngân",
-  email: "thungan01@gmail.com",
-  phone: "0988 123 456",
   created: "01/12/2025",
 };
 
@@ -12,8 +10,6 @@ const cashierAccount = {
 document.getElementById("accName").innerText = cashierAccount.name;
 document.getElementById("accUsername").innerText = cashierAccount.username;
 document.getElementById("accRole").innerText = cashierAccount.role;
-document.getElementById("accEmail").innerText = cashierAccount.email;
-document.getElementById("accPhone").innerText = cashierAccount.phone;
 document.getElementById("accCreated").innerText = cashierAccount.created;
 
 function goChangePass() {
--- pages/kitchen/kitchen-account.js
+++ pages/kitchen/kitchen-account.js
@@ -3,8 +3,6 @@ const cashierAccount = {
   name: "Huỳnh Kim Cương",
   username: "bep",
   role: "Bếp ",
-  email: "bep01@gmail.com",
-  phone: "0988 0556 366",
   created: "30/12/2025",
 };
 
@@ -12,8 +10,6 @@ const cashierAccount = {
 document.getElementById("accName").innerText = cashierAccount.name;
 document.getElementById("accUsername").innerText = cashierAccount.username;
 document.getElementById("accRole").innerText = cashierAccount.role;
-document.getElementById("accEmail").innerText = cashierAccount.email;
-document.getElementById("accPhone").innerText = cashierAccount.phone;
 document.getElementById("accCreated").innerText = cashierAccount.created;
 
 function goChangePass() {
